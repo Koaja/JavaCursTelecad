a7a0a9e8951c4cd642180e7d0f7d6c4a710258a0
BaleaCristi <balea.cristian90@gmail.com>
2015-11-25 17:19:11 +0200

    minor changes to examen partial

--- Java1Recap/src/ExempluExamenPartial.java
+++ Java1Recap/src/ExempluExamenPartial.java
@@ -1,37 +1,39 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class ExempluExamenPartial{
-    public static void main(String [] args){
-     
-        Magazin magazin = new Magazin();
-        
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+public class ExempluExamenPartial {
+	public static void main(String[] args) {
 
-        String line = "";
-        try{
-            while(true){
-                line = in.readLine();
-                String [] array = line.split(" ");
-                
-                if(array[0].equals("adauga")){
-                    String nume = array[1];
-                    double pret = Double.parseDouble(array[2]);
-                    
-                    Produs p = new Produs(nume, pret);
-                    magazin.adaugaProdus(p);
-                }
-                
-                if(array[0].equals("afiseaza")){
-                    magazin.afiseazaProdus();
-                }
-                
-                if(array[0].equals("exit")){
-                    System.exit(0);
-                }
-                                
-            }
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-    }
+		Magazin magazin = new Magazin();
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		String line = "";
+		try {
+			while (true) {
+				line = in.readLine();
+				String[] array = line.split(" ");
+
+				if (array[0].equals("adauga")) {
+					String nume = array[1];
+					double pret = Double.parseDouble(array[2]);
+
+					Produs p = new Produs(nume, pret);
+					magazin.adaugaProdus(p);
+				}
+
+				if (array[0].equals("afiseaza")) {
+					magazin.afiseazaProdus();
+				}
+
+				if (array[0].equals("exit")) {
+					System.exit(0);
+				}
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
