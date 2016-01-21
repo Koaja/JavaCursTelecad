748ab1926fb1b2143425ce1e82ed3e1c834d6bcb
BaleaCristi <balea.cristian90@gmail.com>
2016-01-21 15:04:07 +0200

    .

--- Java1Curs8/src/Fereastra5.java
+++ Java1Curs8/src/Fereastra5.java
@@ -1,38 +1,50 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class Fereastra5 extends JFrame{
-   JButton b1, b2, b3, b4, b5;
-   
-   public Fereastra5(){
-       setTitle("Multiple Buttons");
-       setSize(600,600);
-       setLocationRelativeTo(null);
-       setLayout(new BorderLayout());
-       
-       /*
-        * CENTER , NORTH , SOUTH , EAST , WEST
-        */
-       b1 = new JButton("Cartman");
-       add(b1, BorderLayout.CENTER);
-       
-       b2 = new JButton("Kenny");
-       add(b2, BorderLayout.NORTH);
-       
-       b3 = new JButton("Stan");
-       add(b3, BorderLayout.SOUTH);
-       
-       b4 = new JButton("Kyle");
-       add(b4, BorderLayout.EAST);
-       
-       b5 = new JButton("Butters");
-       add(b5, BorderLayout.WEST);
-       
-       setVisible(true);
-    }
-    
-    public static void main(String [] args){
-        new Fereastra5();
-    }
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Fereastra5 extends JFrame {
+	JButton b1, b2, b3, b4, b5;
+
+	public Fereastra5() {
+		setTitle("Multiple Buttons");
+		setSize(600, 600);
+		setLocationRelativeTo(null);
+		setLayout(new BorderLayout());
+
+		/*
+		 * CENTER , NORTH , SOUTH , EAST , WEST
+		 */
+		b1 = new JButton("Cartman");
+		add(b1, BorderLayout.CENTER);
+
+		b1.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent ev) {
+				if (ev.getClickCount() == 3) {
+					b1.setText("multiple pressed");
+				}
+			}
+
+		});
+
+		b2 = new JButton("Kenny");
+		add(b2, BorderLayout.NORTH);
+
+		b3 = new JButton("Stan");
+		add(b3, BorderLayout.SOUTH);
+
+		b4 = new JButton("Kyle");
+		add(b4, BorderLayout.EAST);
+
+		b5 = new JButton("Butters");
+		add(b5, BorderLayout.WEST);
+
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Fereastra5();
+	}
 }
